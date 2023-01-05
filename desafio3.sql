cb1ff34c331677ee3643abbf80d89f538798bb6b
aires1294 <pedrotja.engenharia@gmail.com>
2023-01-05 01:38:14 -0300

    item 3 feitooo

---
+++ desafio3.sql
@@ -0,0 +1,14 @@
+-- SELECT (SELECT DISTINCT (id_user) FROM SpotifyClone.user) AS usuario,
+-- (SELECT COUNT (id_user) FROM SpotifyClone.playback_history) AS qt_de_musicas_ouvidas,
+-- (SELECT ROUND SUM () )
+SELECT
+    id_user AS usuario ,
+    COUNT (id_user) AS qt_de_musicas_ouvidas,
+    ROUND (SUM(sec_duration) / 60, 2) AS total_minutos
+FROM 
+    SpotifyClone.user AS u
+INNER JOIN SpotfyCiclone.playback_history AS p ON u.id_user = p.ud_user
+INNER JOIN SpotfyCiclone.music AS m ON m.id_music = p.id_music
+GROUP BY user
+ORDER BY user ASC;
+ 
