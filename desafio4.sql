792ff2f0b5d593e16a0f657a97bd96e650cf9616
aires1294 <pedrotja.engenharia@gmail.com>
2023-01-05 22:54:05 -0300

    item 4 feitooo

--- desafio4.sql
+++ desafio4.sql
@@ -1,9 +1,6 @@
-SELECT 
-    DISTINCT u.user usuario,
-	IF(YEAR(MAX(p.reproduction_date)) >= 2021,'Usuário ativo','Usuário inativo') status_usuario
-FROM SpotifyClone.playback_history p
-INNER JOIN SpotifyClone.user u ON p.user_id = u.user_id
-GROUP BY u.user
-ORDER BY u.user;
-
-
+SELECT u.user AS "usuario",
+    IF(MAX(YEAR(reproduction_date)) >= 2021, 'Usuário ativo', 'Usuário inativo') AS "status_usuario"
+FROM SpotifyClone.user AS u
+    INNER JOIN SpotifyClone.playback_history AS p ON u.id_user = p.id_user
+GROUP BY usuario
+ORDER BY usuario;
